31cd83c0469be4fbf49a9e8e6a1c8eedb6d2902c
joe <musketeerdt@gmail.com>
2024-11-15 17:44:24 +0800

    correct cross entropy

--- src/crossEntropy.py
+++ src/crossEntropy.py
@@ -2,7 +2,7 @@ import numpy as np
 class crossEntropy(object):
     @staticmethod
     def fn(a,y):
-        return -np.sum(y(np.log(a)+(1-y)*np.log(1-a)))
+        return -np.sum(y*(np.log(a))+(1-y)*np.log(1-a))
     
     @staticmethod
     def delta(z,a,y):
