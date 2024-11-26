3982c070d3a4cb53ca4b436dc5d0936f6369b7db
joe <musketeerdt@gmail.com>
2024-11-26 22:09:36 +0800

    tailored train code

--- src/train.py
+++ src/train.py
@@ -5,46 +5,22 @@ import numpy as np
 from scipy.io import loadmat
 import time
 import pandas as pd
-import crossEntropy
 import softmax
 from sklearn.preprocessing import MinMaxScaler
 
-def my_transform(mydata):
-    mydata=mydata.to_numpy()
-    x=mydata[:,:7]
-    scaler = MinMaxScaler()
-    x=scaler.fit_transform(x)
-    y=mydata[:,7]
-    y=y.reshape(len(y),1)
-    x=x.tolist()
-    for i in range(len(x)):
-        x[i]=np.array(x[i])
-        tmp=np.zeros((15,1))
-        tmp[int(y[i][0])]=np.array([1])
-        x[i]=[np.reshape(x[i],(7,1)),tmp]
-    return x
 
-if __name__=="__main__":
-    # data = loadmat('data/mnist-original.mat')['data'].transpose()/255.0
-    # label = loadmat('data/mnist-original.mat')['label'].transpose()
-    # data=data.tolist()
-    # for i in range(len(data)):
-    #     data[i]=np.array(data[i])
-    #     tmp=np.zeros((10,1))
-    #     tmp[int(label[i][0])]=np.array([1])
-    #     data[i]=[np.reshape(data[i],(784,1)),tmp]
-        
-    # my_testdata=data[60000:]
-    
-    # training_data , validation_data , test_data = mnist_loader.load_data_wrapper()
-    mydata=pd.read_csv("data/real_train.csv")
-    my_test_data=pd.read_csv("data/real_test.csv")
-    x=my_transform(mydata)
-    test=my_transform(my_test_data)
-    net = network.Network(sizes=(7,150,15),cost=softmax.softmax)
-    # net=network.load("data/save/model.json")
-    # all_data=training_data+data[:60000]
-    # all_data=data
-    # all_testdata=test_data+my_testdata
-    net.SGD(x , 45, 1000, 0.3, evaluationData=test,lmda=500,momentum_coefficient=0.5)
-    
+if __name__ == "__main__":
+    data = loadmat('data/mnist-original.mat')['data'].transpose()/255.0
+    label = loadmat('data/mnist-original.mat')['label'].transpose()
+    data=data.tolist()
+    for i in range(len(data)):
+        data[i]=np.array(data[i])
+        tmp=np.zeros((10,1))
+        tmp[int(label[i][0])]=np.array([1])
+        data[i]=[np.reshape(data[i],(784,1)),tmp]
+
+    my_testdata=data[60000:]
+
+    net = network.Network(sizes=(784,150, 10), cost=network.crossEntropy)
+    net.SGD(data[:60000], 45, 50, 1, evaluationData=my_testdata, lmda=1, momentum_coefficient=0.2,
+            monitor_training_cost=False, monitor_training_accuracy=False)
--- src/train3.py
+++ src/train3.py
@@ -1,21 +1,25 @@
-import network3
-from network3 import Network
-from network3 import ConvPoolLayer , FullyConnectedLayer , SoftmaxLayer
+import mynetwork3
+from mynetwork3 import Network,relu
+from mynetwork3 import FullyConnectedLayer , SoftmaxLayer,CrossEntropyLayer,ConvPoolLayer
 if __name__ == "__main__":
-    training_data , validation_data , test_data = network3.load_data_shared("data/mnist.pkl.gz")
-    mini_batch_size = 10
+    training_data , validation_data , test_data = mynetwork3.load_data_shared("data/mnist_expanded.pkl.gz")
+    mini_batch_size = 1000
     net = Network ([
-        ConvPoolLayer (
-            image_shape = ( mini_batch_size , 1, 28, 28) ,
+            ConvPoolLayer (
+            image_shape =( mini_batch_size , 1, 28, 28) ,
             filter_shape =(20 , 1, 5, 5),
-            poolsize =(2, 2)
-        ),
-        ConvPoolLayer (
+            poolsize =(2, 2),
+            activation_fn=relu
+            ),
+            ConvPoolLayer (
             image_shape =( mini_batch_size , 20, 12, 12) ,
             filter_shape =(40 , 20, 5, 5),
-            poolsize =(2, 2)
-        ),
-        FullyConnectedLayer (n_in =40*4*4 , n_out =100) ,
+            poolsize =(2, 2),
+            activation_fn=relu
+            ),
+            FullyConnectedLayer (n_in =40*4*4 , n_out =100,activation_fn=relu,p_dropout=0.2) ,
+            FullyConnectedLayer (n_in =100, n_out =100,activation_fn=relu,p_dropout=0.2) ,
+        ],
         SoftmaxLayer (n_in =100 , n_out =10)
-    ], mini_batch_size )
-    net.SGD(training_data , 60, mini_batch_size , 0.1, validation_data , test_data )
+    )
+    net.SGD(training_data , validation_data , test_data,50,mini_batch_size,0.1,1,0.2 )
